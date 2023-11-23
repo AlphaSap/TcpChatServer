0b9fdfee359701fc87a329e50b0d29848f46a26e
sahil <sahilpanchal2903@gmail.com>
2023-11-23 16:46:10 +0000

    added message type

--- chat_messages/src/lib.rs
+++ chat_messages/src/lib.rs
@@ -5,11 +5,16 @@ use serde::Serialize;
 #[derive(serde::Deserialize, Debug, Serialize)]
 pub struct Message {
     message: String,
+    name: String,
 }
 
 impl Message {
-    pub fn new(message: String) -> Self {
-        Self { message }
+    pub fn new(message: String, name: String) -> Self {
+        Self { message , name }
+    }
+
+    pub fn name(&self) -> &String {
+        &self.name
     }
 
     pub fn message(&self) -> &String {
